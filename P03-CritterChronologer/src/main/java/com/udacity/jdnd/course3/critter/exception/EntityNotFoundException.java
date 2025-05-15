a400a8c357fb81e45d96373b31ad4e63c0a22c6d
Jormart <jmartineztortola@gmail.com>
2025-05-15 08:16:36 +0200

    first commit

--- P03-CritterChronologer/src/main/java/com/udacity/jdnd/course3/critter/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.udacity.jdnd.course3.critter.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
-
